55d58a7419e715fb87f5cbe2e0c7de8cbbc34897
mene135 <menelaoss027@gmail.com>
2024-05-25 18:25:13 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    mode: "development",
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env', { targets: "defaults" }]
+                    ]
+                  }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
